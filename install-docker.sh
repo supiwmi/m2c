eefdd22c367ed95d3298b2f3a0e02773fcb3ecf7
supiwmi <35697929+supiwmi@users.noreply.github.com>
2020-11-23 19:19:50 +0700

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,1 +1,15 @@
-d
+!#/bin/bash
+
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo systemctl status docker
+sudo systemctl enable docker
+sudo usermod -aG docker ${USER}
+su - ${USER}
+id -nG
+sudo usermod -aG docker username
+docker run hello-world
