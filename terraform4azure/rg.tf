b358a43f4da5a2d9b0a78ebe0a844fb330325152
supiwmi <35697929+supiwmi@users.noreply.github.com>
2020-11-23 20:15:32 +0700

    Create rg.tf

---
+++ terraform4azure/rg.tf
@@ -0,0 +1,24 @@
+# Configure the Azure Provider
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "=2.20.0"
+
+  subscription_id = "69c3--------------------------------"
+  tenant_id       = "4d73--------------------------------"
+
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "Southeast Asia"
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
+}
